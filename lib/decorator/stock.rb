ac5fbe088ecb117e27fcd0de844a16a9f96928ed
Stephen McCullough <stephen@sixdegreelabs.com>
2015-10-05 14:17:45 +0100

    SPIKING - Adding ModifierDecorator to see if that will work

--- lib/decorator/stock.rb
+++ lib/decorator/stock.rb
@@ -1,7 +1,17 @@
 require 'representable/json'
 
+class ModifierDecorator < Representable::Decorator
+  include Representable::JSON
+
+  property :name
+  property :price
+end
+
 class StockDecorator < Representable::Decorator
   include Representable::JSON
 
   property :id
+
+  collection :modifiers, extend: ModifierDecorator
 end
+
